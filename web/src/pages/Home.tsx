3322309f829e41c3c828dc0e5a6a93bae5f73a60
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2022-06-18 13:50:49 -0400

    created podcast index w links

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,27 @@
 import { tw } from 'twind';
 import { Podcast } from 'project-shared';
-import { getPodcastsByTags } from '../utils';
+import { getPodcasts, getPodcastsByTags } from '../utils';
+import { useEffect, useState } from 'react';
+import useParse from '../hooks/useParse';
+import { PodcastCard } from '../components/PodcastCard';
 
 const HomePage: React.FC = () => {
+  const [podcasts, setPodcasts] = useState<Podcast[]>();
+  const { initialized } = useParse();
+
+  useEffect(() => {
+    if (!initialized) return;
+    async function getPodcast() {
+      const podcastResponse = await getPodcasts();
+      setPodcasts(podcastResponse);
+    }
+    getPodcast();
+  }, [initialized]);
+
+  if (!podcasts) return null;
+
+  console.log('podcasts', podcasts);
+
   return (
     <div className={tw(`h-full w-full bg-gray-200 text-blue-500 bg-[#ffffff]`)}>
       <h1>Hello World!</h1>
@@ -14,6 +33,9 @@ const HomePage: React.FC = () => {
       >
         test
       </button>
+      {podcasts.map((podcast) => (
+        <PodcastCard key={podcast.id} podcast={podcast} />
+      ))}
     </div>
   );
 };
