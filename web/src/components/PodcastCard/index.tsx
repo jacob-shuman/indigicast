7f7570e7ca0744680473a2778faa329cb70b1a7e
Meep <mwraich28@gmail.com>
2022-06-18 15:27:05 -0400

    Styles

---
+++ web/src/components/PodcastCard/index.tsx
@@ -0,0 +1,46 @@
+import { Podcast } from 'project-shared';
+import { tw } from 'twind';
+import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row } from 'reactstrap';
+import { Col } from 'reactstrap';
+
+interface PodcastCardProps {
+  podcast: Podcast;
+}
+export const PodcastCard: React.FC<PodcastCardProps> = ({ podcast }) => {
+  const { attributes, createdAt, updatedAt } = podcast;
+  const { name, tags, file } = attributes;
+  const imageUrl = file?._url ? file._url : 'https://unsplash.com/photos/78A265wPiO4'
+  const recordedOn = attributes.recordedDate
+
+  return (
+      <Card>
+        <CardBody>
+          <Row>
+        <Col>
+        <CardTitle><h1>{name}</h1></CardTitle>
+          <p>{tags.map((tag) => tag)}</p>
+          <CardText>
+          <p>Created on: {format(createdAt, 'MM/dd/yyyy')}</p>
+          {recordedOn && 
+            <p>Recorded on: {format(recordedOn, 'MM/dd/yyyy')}</p>
+          }
+          </CardText>g
+        </Col>
+        <Col>
+        <Link to={`podcasts/${podcast.id}`} >
+          <CardImg src={imageUrl}></CardImg>
+        </Link>
+        </Col>
+        </Row>
+        <Button color='primary'>
+        <Link to={`podcasts/${podcast.id}`} style={{color: 'white'}}>
+          Listen to Podcast
+          </Link>
+        </Button>
+        </CardBody>
+      </Card>
+  );
+};
